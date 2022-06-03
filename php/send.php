6045744e5251f6f7db27e4a898f52862421e0c03
cpadridev <cpadridev@gmail.com>
2022-06-03 15:21:02 +0200

    Prueba enviar formulario, actualizacion en las habilidades

---
+++ php/send.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$affair = $_POST['affair'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Asunto: " . $affair . " \r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'cpadridev@gmail.com';
+$asunto = 'Mensaje de contacto desde el portfolio';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:ty.html");
+?>
